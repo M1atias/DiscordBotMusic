fc1f5713eef88ac3e781ed5c46b998a1bd5175df
M1atias <maty.f.molina4a@gmail.com>
2021-12-08 20:35:11 -0300

    add: se comenzo con el bot de musica

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const discord = require('discord.js');
+
+const client = new discord.Client();
+
+client.on('ready', () =>{
+    console.log('Online')
+})
+
+client.login('OTE4Mjc5MjcyMDY4NjgxNzM5.YbE8Nw.dxCT3jZe2X2QTYUDiIYzdVosoLo')
